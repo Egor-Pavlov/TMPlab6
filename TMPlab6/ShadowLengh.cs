93df250bcdf364e604be471e5d333e7ca1625413
Егор <EgorPavlov2001@yandex.ru>
2021-04-26 13:38:52 +0700

    Добавьте файлы проекта.

---
+++ TMPlab6/ShadowLengh.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMPlab6
+{
+    class ShadowLengh
+    {
+        List<(double, double)> lines = new List<(double, double)>();
+        public ShadowLengh(List<(double, double)> lines)
+        {
+            this.lines = lines;
+        }
+        public double CalcLengh()
+        {
+            double sum = 0;
+            for(int i = 0; i < lines.Count; i++)
+            {
+                sum += (lines[i].Item2 - lines[i].Item1);
+            }
+            return sum;
+        }
+    }
+}
---
+++ TMPlab6/TypeOfGraphic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TMPlab6
+{
+    class TypeOfGraphic
+    {
+        static string Define(List<double> Coeffs) //лист хранит коэффициенты ABCDEF
+        {
+            double D = 0, d = 0;
+            if(Coeffs.Count != 6)
+            {
+                throw new ArgumentException();
+            }
+
+            D = Coeffs[0] * Coeffs[2] * Coeffs[5] + Coeffs[1] * Coeffs[4] * Coeffs[3] + Coeffs[1] * Coeffs[4] * Coeffs[3] -
+                Coeffs[3] * Coeffs[2] * Coeffs[3] - Coeffs[1] * Coeffs[1] * Coeffs[5] - Coeffs[0] * Coeffs[4] * Coeffs[4];
+            d = Coeffs[0] * Coeffs[2] - Coeffs[1] * Coeffs[1];
+            
+            if(D == 0)
+            {
+                return "какой-то вид";
+            }
+            else if(d < 0)
+            {
+                return "Гипербола";
+            }
+            else if (d > 0)
+            {
+                return "Эллипс";
+            }
+            
+            return "Парабола";
+            
+        }
+    }
+}
